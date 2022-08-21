ed7de1e175adce65bc039e879a6684de4e837605
Luiz Abbott <luiz_abbott@epam.com>
2022-08-20 23:33:43 -0300

    improvement: removing db dependencies from handler

--- product-service/src/functions/getProductsList/handler.ts
+++ product-service/src/functions/getProductsList/handler.ts
@@ -1,5 +1,4 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
-import { AppDataSource } from '../../data-source';
 import { formatJSONResponse } from '../../libs/api-gateway';
 import { middyfy } from '../../libs/lambda';
 import { getProductsListCall } from '../../services/product.service';
@@ -7,16 +6,16 @@ import { getProductsListCall } from '../../services/product.service';
 export const getProductsList: APIGatewayProxyHandler = async () => {
   console.log(`Lambda: getProductsList()`);
 
-  return await AppDataSource.initialize().then(async () => {
-      const products = await getProductsListCall();
-      return formatJSONResponse({ products });
-    }).catch(error => {
-      console.log(error);
-      return {
-        statusCode: 500,
-        body: JSON.stringify("Internal Server Error")
-      }
-  }).finally(() => AppDataSource.destroy());
+  try {
+    const products = await getProductsListCall();
+    return formatJSONResponse({ products });
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify("Internal Server Error")
+    }
+  }
 };
 
 export const main = middyfy(getProductsList);
--- product-service/src/services/product.service.ts
+++ product-service/src/services/product.service.ts
@@ -7,35 +7,50 @@ import { StockEntity } from "../entity/Stock";
 import { convertToProductModel } from "../utils/utils";
 
 export const getProductCall = async (productId: string): Promise<Product> => {
-  const product = await AppDataSource.manager.findOne(ProductEntity, {
-    relations: { stock: true },
-    where: { id: productId }
-  });
-  if (!product) {
-    return Promise.reject("Product not found");
-  }
-  return convertToProductModel(product);
+  return await AppDataSource.initialize()
+  .then(async () => {
+    const product = await AppDataSource.manager.findOne(ProductEntity, {
+      relations: { stock: true },
+      where: { id: productId }
+    });
+    if (!product) {
+      return Promise.reject("Product not found");
+    }
+    return convertToProductModel(product);
+  })
+  .catch(error => error)
+  .finally(() => AppDataSource.destroy());
 }
 
 export const getProductsListCall = async (): Promise<Product[]> => {
-  const products = await AppDataSource.manager.find(ProductEntity, {
-    relations: { stock: true },
-  });
-
-  return products.map(product => convertToProductModel(product));   
+  return await AppDataSource.initialize()
+  .then(async () => {
+    const products = await AppDataSource.manager.find(ProductEntity, {
+      relations: { stock: true },
+    });
+  
+    return products.map(product => convertToProductModel(product));   
+  })
+  .catch(error => error)
+  .finally(() => AppDataSource.destroy());
 }
 
 export const createProductCall = async (product: Product): Promise<void> => {
-  const { id = randomUUID(), title, description, count, price } = product;
+  return await AppDataSource.initialize()
+  .then(async () => {
+    const { id = randomUUID(), title, description, count, price } = product;
   
-  if (isNotValidProductBody({ id, title, description, count, price })) {
-    return Promise.reject("Product data is invalid");
-  }
-  
-  return await AppDataSource.manager.transaction(async (transactionalEntityManager) => {
-    await transactionalEntityManager.save(ProductEntity, { id, title, description, price });
-    await transactionalEntityManager.save(StockEntity, { product_id: id, count });
-  }).catch(error => Promise.reject(error));
+    if (isNotValidProductBody({ id, title, description, count, price })) {
+      return Promise.reject("Product data is invalid");
+    }
+    
+    return await AppDataSource.manager.transaction(async (transactionalEntityManager) => {
+      await transactionalEntityManager.save(ProductEntity, { id, title, description, price });
+      await transactionalEntityManager.save(StockEntity, { product_id: id, count });
+    }).catch(error => Promise.reject(error));
+  })
+  .catch(error => error)
+  .finally(() => AppDataSource.destroy());
 }
 
 const isNotValidProductBody = (product: Product): boolean => {
